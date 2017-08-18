75cffbdc0539678f5bb15f12d752d7d5c8065669
madlaina-kalunder <kalunder@archilogic.com>
2017-08-18 09:09:05 +0200

    (WIP)

---
+++ app.js
@@ -0,0 +1,19 @@
+// Element references
+
+var originalView = document.querySelector('#original-view')
+var modifiedView = document.querySelector('#modified-view')
+
+var backButton = document.querySelector('#back-button')
+var preview = document.querySelector('#preview')
+var previewLoadingOverlay = document.querySelector('#preview-loading-overlay')
+
+function updateModifiedView () {
+
+}
+
+function hideElement (elem) {
+  elem.style.display = 'none'
+}
+
+hideElement(previewLoadingOverlay)
+hideElement(backButton)
