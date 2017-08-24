f55b2c639bf17249865f4574c5c414bcf66e0cde
madlaina-kalunder <kalunder@archilogic.com>
2017-08-24 15:20:41 +0200

    (WIP)  3dio modify extension draft

--- 3d-io-extension.js
+++ 3d-io-extension.js
@@ -1,17 +1,19 @@
-// Extensions for 3dio-js
+// extend io3d.js library until "io3d." is ready
 
-// making modify api calls
-export default function modify (key, options) {
-  var defaultOptions = {}
-  var modifyOptions = options || defaultOptions
-  
-  return callService('Processing.task.enqueue', {
-    method: 'modify',
-    params: {
-      inputFileKey: '535e624259ee6b0200000484/170818-1159-hwaumn/archilogic_2017-08-18_11-59-29_5kPrUk.gz.data3d.buffer'
-      options: {
-        modifiers: modifyOptions
-      }
-    }
-  })
+io3d.fish = io3d.model || {}
+
+io3d.fish.modify = function modify (options) {
+    var defaultOptions = ['origami']
+    var options = options || defaultOptions
+
+    return io3d.utils.services.call('Processing.task.enqueue', {
+        method: 'modify',
+        params: {
+            inputFileKey: '535e624259ee6b0200000484/170818-1159-hwaumn/archilogic_2017-08-18_11-59-29_5kPrUk.gz.data3d.buffer',
+            outputDirectory: '535e624259ee6b0200000484/170824-madlaina-test-' + Date.now(),
+            options: {
+                modifiers: options
+            }
+        }
+    })
 }
